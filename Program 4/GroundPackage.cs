55c6745bbf7015f1b1f7e342c56720ee5c623d06
Hsiaffa <hiltonsiaffa@gmail.com>
2020-05-31 18:50:22 -0400

    Add project files.

---
+++ Program 4/GroundPackage.cs
@@ -0,0 +1,185 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Program_4
+{
+   
+    
+        public class GroundPackage
+        {
+            private int _aZipcode; // Package Origin 
+            private int _bZipcode; // Package destination
+            private double _length; // Package length
+        private double _width; // Package width
+        private double _height; // Package height
+            private double _weigth; // Package weight
+            const int OriginZip = 40204; // Dafault Zipcode
+            const int DestinationZip = 60606; //default destination
+            const int myset = 12; //Default Dimension value
+
+
+            public GroundPackage(int aZipCode, int bZipcode, double length, double width, double height, double weight)
+            {
+            // use properties to ensure validation occurs
+                AZipcode = aZipCode;
+                BZipcode = bZipcode;
+                Length = length;
+                Width = width;
+                Height = height;
+                Weight = weight;
+
+            }
+            public int AZipcode
+            {
+            //Precondition: None
+            // Postcondition: The package origin zip code has been returned
+                get
+                {
+                    return _aZipcode;
+                }
+            // Precondition: value >= 00000 and value <= 99999
+            //Postcondition: The package's origin zip code has been set to the specified value
+            set
+            {
+                if (value >= 00000 && value <= 99999)
+                        _aZipcode = value;
+                    else
+                        _aZipcode = OriginZip;
+
+                }
+            }
+            public int BZipcode
+            {
+            //Precondition: None
+            // Postcondition: The package destination zip code has been returned
+            get
+            {
+                    return _bZipcode;
+                }
+            // Precondition: value >= 00000 and value <= 99999
+            //Postcondition: The package's origin zip code has been set to the specified value
+            set
+            {
+                    if (value >= 00000 && value <= 99999)
+                        _bZipcode = value;
+                    else
+                        _bZipcode = DestinationZip;
+                }
+            }
+            public double Length
+            {
+            //Precondition: None
+            // Postcondition: The package length has been returned
+            get
+            {
+                    return _length;
+            }
+            // Precondition: value > 0 
+            //Postcondition: The package's length has been set to the specified value
+            set
+            {
+                    if (value > 0)
+                        _length = value;
+                    else
+                        _length = myset;
+                }
+            }
+            public double Width
+            {
+            //Precondition: None
+            // Postcondition: The package width has been returned
+            get
+            {
+                    return _width;
+                }
+            // Precondition: value > 0 
+            //Postcondition: The package's width has been set to the specified value
+            set
+            {
+                    if (value > 0)
+                        _width = value;
+                    else
+                        _width = myset;
+                }
+            }
+            public double Height
+            {
+            //Precondition: None
+            // Postcondition: The package height has been returned
+            get
+            {
+                    return _height;
+                }
+            // Precondition: value > 0 
+            //Postcondition: The package's height has been set to the specified value
+            set
+            {
+                    if (value > 0)
+                        _height = value;
+                    else
+                        _height = myset;
+                }
+            }
+            public double Weight
+            {
+            //Precondition: None
+            // Postcondition: The package weigth has been returned
+            get
+            {
+                    return _weigth;
+                }
+            // Precondition: value > 0 
+            //Postcondition: The package's weight has been set to the specified value
+            set
+            {
+                    if (value > 0)
+                        _weigth = value;
+                    else
+                        _weigth = myset;
+                }
+            }
+            public int ZoneDistance
+            {
+            //Precondition: None
+            // Postcondition: The package zone distence has been returned
+            get
+            {
+                    const int MY_NUM = 10000; //Denominator to extract 1st digit
+                    int mydiff; // calculated zone difference
+
+                    mydiff = (_aZipcode / MY_NUM) - (_bZipcode / MY_NUM);
+
+                    return Math.Abs(mydiff); // Absolute value in case negative
+                }
+            }
+
+            public double CalcCost()
+            {
+            //Precondition: None
+            // Postcondition: The package cost has been returned
+            const double MY_DINUM = .20; // Dimension coefficient in cost equation
+                const double WEIGHT_IMP = .5; // weight coefficient in cost equation
+
+                return (MY_DINUM * (Length + Width + Height) + WEIGHT_IMP * (ZoneDistance + 1) * Weight);
+            }
+            public override string ToString()
+            {
+            
+            string PACKAGE_INFO;
+
+                PACKAGE_INFO = $"OriginZip: {OriginZip}" + Environment.NewLine;
+                PACKAGE_INFO += $"DestinationZip: {DestinationZip}" + Environment.NewLine;
+                PACKAGE_INFO += $"Length: {Length}" + Environment.NewLine;
+                PACKAGE_INFO += $"Width: {Width}" + Environment.NewLine;
+                PACKAGE_INFO += $"Height: {Height}" + Environment.NewLine;
+                PACKAGE_INFO += $"Weight: {Weight}" + Environment.NewLine;
+
+                return PACKAGE_INFO;
+
+            }
+        }
+}
+
---
+++ Program 4/Program.cs
@@ -0,0 +1,51 @@
+﻿// 199-75
+// Grading ID: 1566
+// Due: April 20, 2020
+//Program 4
+// A Console application with two classes, One class will be the test application where the Main method is located and
+//the other public class is named GroundPackage and will be our initial attempt at representing some of the packages delivered by Brown Parcel Service.
+using static System.Console;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Program_4
+{
+  public class Program
+    {
+        static void Main(string[] args)
+        {
+            GroundPackage[] packages = new GroundPackage[6];
+            packages[0] = new GroundPackage(48794, 65656, 8, 7, 6, 2);
+            packages[1] = new GroundPackage(98989, 54649, 6, 9, 7, 4);
+            packages[2] = new GroundPackage(98795, 64656, 4, 4, 6, 1);
+            packages[3] = new GroundPackage(99999, 56214, 2, 6, 9, 8);
+            packages[4] = new GroundPackage(98999, 33256, 6, 9, 6, 7);
+            packages[5] = new GroundPackage(64649, 64849, 9, 6, 7, 1);
+
+            DisplayPackages(packages);
+
+            // Changing the 
+            packages[0].AZipcode = 64646;
+            packages[1].BZipcode = 99999;
+            packages[2].Length = 1;
+            packages[3].Width = 2;
+            packages[4].Height = 5;
+            packages[4].Weight = 6;
+
+            DisplayPackages(packages); // Display Package method
+        }
+        public static void DisplayPackages(GroundPackage[] packages)
+        {
+            foreach(GroundPackage a in packages)
+            {
+                // output
+                WriteLine(a.ToString());
+                double sum = a.CalcCost(); 
+                WriteLine(sum); 
+            }
+        }
+    }
+}
